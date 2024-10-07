6462cc02b9b7729d66490a0d5ea688ee8b9c2f15
Mauricio Sosa <msosa@imobile3.com>
2024-10-07 14:40:56 -0400

    Initial Commit

---
+++ MPRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPRecipe
+//
+//  Created by Mauricio Pimienta on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
